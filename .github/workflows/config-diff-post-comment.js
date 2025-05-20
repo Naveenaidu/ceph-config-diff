4bf7789db3cc3e7eba923acb60f30c5e2f4ca12f
Naveen Naidu <naveennaidu479@gmail.com>
2025-05-20 18:04:11 +0530

    test posix-diff

--- .github/workflows/config-diff-post-comment.js
+++ .github/workflows/config-diff-post-comment.js
@@ -1,23 +1,23 @@
-module.exports = async ({ github, context, core, diffJson }) => {
+module.exports = async ({ github, context, core, configDiff }) => {
     try {
         // Check if the structure matches {"added": {}, "deleted": {}, "modified": {}}
         // Do not create comment if there are no configuration changes
-        const isEmptyDiff = Object.keys(diffJson.added).length === 0 &&
-          Object.keys(diffJson.deleted).length === 0 &&
-          Object.keys(diffJson.modified).length === 0;
+        // const isEmptyDiff = Object.keys(configDiff.added).length === 0 &&
+        //   Object.keys(configDiff.deleted).length === 0 &&
+        //   Object.keys(configDiff.modified).length === 0;
     
-        if (isEmptyDiff) {
+        if (!configDiff) {
           console.log("No changes detected. Skipping comment creation.");
           return;
         }
     
-        const diffOutput = JSON.stringify(diffJson, null, 2);
+        // const diffOutput = JSON.stringify(configDiff, null, 2);
         const commentBody = `
 ### Config Diff Tool Output
 
-\`\`\`json
+\`\`\`
 
-${diffOutput}
+${configDiff}
 
 \`\`\`
   
@@ -59,7 +59,7 @@ The above configuration changes are found in the PR. Please update the relevant 
         });
     
         // Set the status as FAILED if any configuration changes are detected
-        console.log("Configuration changes detected: ",  diffOutput);
+        console.log("Configuration changes detected: ",  configDiff);
         core.setFailed("Configuration Changes Detected, Update release documents - if necessary");
       } catch (error) {
         core.setFailed(error.message);
